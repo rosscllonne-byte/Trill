38f521304ca1544cbbb8852d440ca0e1b980f484
rosscllonne-byte <rosscllonne@gmail.com>
2025-10-05 17:19:09 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4f46e5',
+        secondary: '#0ea5e9',
+      },
+    },
+  },
+  plugins: [],
+}
